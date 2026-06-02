638472557df0d3e056c485091c72125acd05d910
William Wong <compulim@users.noreply.github.com>
2026-06-02 08:24:43 +0000

    Remove browser unit tests

--- packages/selenium-webdriver-message-port/src/browser/index.test.ts
+++
@@ -1,6 +0,0 @@
-import '@happy-dom/global-registrator/register.js';
-import { GlobalRegistrator } from '@happy-dom/global-registrator';
-
-import './index.ts';
-
-await GlobalRegistrator.unregister();
