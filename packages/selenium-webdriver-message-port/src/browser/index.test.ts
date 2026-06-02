83a58e3b3e007abdcc73253f3bd2d27355d53f0b
William Wong <compulim@users.noreply.github.com>
2026-06-02 08:11:41 +0000

    Add happy-dom

--- packages/selenium-webdriver-message-port/src/browser/index.test.ts
+++ packages/selenium-webdriver-message-port/src/browser/index.test.ts
@@ -1,1 +1,6 @@
+import '@happy-dom/global-registrator/register.js';
+import { GlobalRegistrator } from '@happy-dom/global-registrator';
+
 import './index.ts';
+
+await GlobalRegistrator.unregister();
